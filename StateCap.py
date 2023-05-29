9afb658c263482d782b4b4e016f349952130c945
Lior Lavi <liorlavi1@gmail.com>
2023-05-29 18:28:46 +0300

    Fixed StateCap.py

--- StateCap.py
+++ StateCap.py
@@ -13,7 +13,38 @@ handle that?
 """
 import sys
 
-import pytest
+STATES_CAPITALS = {
+    'Alabama' : 'Montgomery',
+    'Alaska' : 'Juneau',
+    'Arizona' : 'Phoenix',
+    'Arkansas': 'Little Rock',
+    'California' : 'Sacramento',
+    'Colorado' : 'Denver',
+    'Connecticut' : 'Hartford',
+    'Delaware' : 'Dover',
+    'Florida' : 'Tallahassee',
+    'Georgia' : 'Atlanta',
+    'Hawaii' : 'Honolulu',
+    'Idaho' : 'Boise',
+    'Illinois' : 'Springfield',
+    'Indiana' : 'Indianapolis',
+    'Iowa' : 'Des Moines',
+    'Kansas' : 'Topeka',
+    'Kentucky' : 'Frankfort',
+    'Louisiana' : """We have an existing dictionary that maps US states to their capitals.
+1. Print the state capital of Idaho
+2. Print all states.
+3. Print all capitals.
+4. Create a single string 'Alabama -> Montgomery, Alaska -> Juneau, ...'
+5. Ensure the string you created in 4. is alphabetically sorted by state
+7. Now we want to add the reverse look up, given the name of a capital what state
+is it in?
+Implement the function def get_state(capital): below so it returns the state.
+GOTCHAS: What happens if two states have the same capital name, how do you
+handle that?
+
+"""
+import sys
 
 STATES_CAPITALS = {
     'Alabama' : 'Montgomery',
@@ -70,24 +101,37 @@ STATES_CAPITALS = {
 
 
 def capital_of_Idaho():
-    # Your code here
+    print(STATES_CAPITALS['Idaho'])
     pass
 
 def all_states():
-    # Your code here
+    for keys, values in STATES_CAPITALS.items():
+        print(keys)
     pass
 
 def all_capitals():
-    # Your code here
+    for keys, values in STATES_CAPITALS.items():
+        print(values)
     pass
 
 def states_capitals_string():
-    # Your code here
+    list = []
+    sorted(STATES_CAPITALS)
+    for keys, values in STATES_CAPITALS.items():
+        list.append(keys)
+        list.append('->')
+        list.append(values)
+        list.append(',')
+        newlist = ' '.join(list)
+    print(newlist)
     pass
 
 
 
 def get_state(capital):
+    for keys, values in STATES_CAPITALS.items():
+        if values == capital:
+            print(keys)
     pass
 
 
@@ -96,23 +140,106 @@ def test_state_to_capital():
     assert 'Cheyenne' == STATES_CAPITALS['Wyoming']
 
 
-def test_state_to_capital_unknown():
-    with pytest.raises(KeyError):
-        STATES_CAPITALS['']
+def test_capital_to_state():
+    assert 'Wyoming' == get_state('Cheyenne')
+
+
+print("1.")
+capital_of_Idaho()
+print("2.")
+all_states()
+print("3.")
+all_capitals()
+print("4.")
+states_capitals_string()
+print("5.")
+get_state('Salem')'Baton Rouge',
+    'Maine' : 'Augusta',
+    'Maryland' : 'Annapolis',
+    'Massachusetts' : 'Boston',
+    'Michigan' : 'Lansing',
+    'Minnesota' : 'Saint Paul',
+    'Mississippi' : 'Jackson',
+    'Missouri' : 'Jefferson City',
+    'Montana' : 'Helena',
+    'Nebraska' : 'Lincoln',
+    'Nevada' : 'Carson City',
+    'New Hampshire' : 'Concord',
+    'New Jersey' : 'Trenton',
+    'New Mexico' : 'Santa Fe',
+    'New York' : 'Albany',
+    'North Carolina' : 'Raleigh',
+    'North Dakota' : 'Bismarck',
+    'Ohio' : 'Columbus',
+    'Oklahoma' : 'Oklahoma City',
+    'Oregon' : 'Salem',
+    'Pennsylvania' : 'Harrisburg',
+    'Rhode Island' : 'Providence',
+    'South Carolina' : 'Columbia',
+    'South Dakota' : 'Pierre',
+    'Tennessee' : 'Nashville',
+    'Texas' : 'Austin',
+    'Utah' : 'Salt Lake City',
+    'Vermont' : 'Montpelier',
+    'Virginia' : 'Richmond',
+    'Washington' : 'Olympia',
+    'West Virginia' : 'Charleston',
+    'Wisconsin' : 'Madison',
+    'Wyoming' : 'Cheyenne',
+}
+
+
+def capital_of_Idaho():
+    print(STATES_CAPITALS['Idaho'])
+    pass
+
+def all_states():
+    for keys, values in STATES_CAPITALS.items():
+        print(keys)
+    pass
+
+def all_capitals():
+    for keys, values in STATES_CAPITALS.items():
+        print(values)
+    pass
+
+def states_capitals_string():
+    list = []
+    sorted(STATES_CAPITALS)
+    for keys, values in STATES_CAPITALS.items():
+        list.append(keys)
+        list.append('->')
+        list.append(values)
+        list.append(',')
+        newlist = ' '.join(list)
+    print(newlist)
+    pass
+
+
+
+def get_state(capital):
+    for keys, values in STATES_CAPITALS.items():
+        if values == capital:
+            print(keys)
+    pass
+
+
+
+def test_state_to_capital():
+    assert 'Cheyenne' == STATES_CAPITALS['Wyoming']
 
 
 def test_capital_to_state():
     assert 'Wyoming' == get_state('Cheyenne')
 
 
-def test_capital_to_state_unknown():
-    with pytest.raises(KeyError):
-        get_state('')
-
-
-def main():
-    return pytest.main(__file__)
-
-
-if __name__ == '__main__':
-    sys.exit(main())
+print("1.")
+capital_of_Idaho()
+print("2.")
+all_states()
+print("3.")
+all_capitals()
+print("4.")
+states_capitals_string()
+print("5.")
+get_state('Salem')
